05efde9c60eb6a7881e90bccd2461c63ffea4734
Pavanmenugonda <pavanmenugonda9@gmail.com>
2023-07-08 16:18:09 +0930

    typing Hello

--- script.js
+++ script.js
@@ -43,4 +43,40 @@ document.addEventListener('DOMContentLoaded', function () {
       menuToggle.classList.remove('active');
     }
   });
+
+ /*typeing*/
+ const textElement = document.getElementById('typing-text');
+const texts = [
+  "Hi There! Nice to meet you!",
+  "Welcome to my website!",
+ 
+ 
+];
+let textIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+  if (charIndex < texts[textIndex].length) {
+    textElement.innerHTML += texts[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeText, 100); // Adjust the typing speed by changing the timeout duration (in milliseconds)
+  } else {
+    setTimeout(eraseText, 3000); // Wait for 5 seconds before erasing the text
+  }
+}
+
+function eraseText() {
+  if (charIndex > 0) {
+    textElement.innerHTML = texts[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseText, 50); // Adjust the erasing speed by changing the timeout duration (in milliseconds)
+  } else {
+    textIndex++;
+    if (textIndex === texts.length) {
+      textIndex = 0;
+    }
+    setTimeout(typeText, 100); // Start typing the next text after erasing is complete
+  }
+}
+typeText();
 });
